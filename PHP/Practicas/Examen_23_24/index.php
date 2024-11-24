3e89ff83ed774c23596aae20386b69559a9e26ee
Victor <menaflores.victor@gmail.com>
2024-11-24 18:41:17 +0100

    PHP-> casa

--- PHP/Practicas/Examen_23_24/index.php
+++ PHP/Practicas/Examen_23_24/index.php
@@ -1,4 +1,5 @@
 <?php
+    session_start();  // Iniciar sesión para mantener los datos entre recargas de página
     require "src/funciones_ctes.php";
     //Conectamos con la base
     try{
@@ -13,6 +14,26 @@
         $result_datos_usuarios=mysqli_query($conexion,$consulta);
     }catch(Exception $e){
         die(error_page("Examen2 23-24","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+    }
+
+    // Traemos los datos del profesor en concreto que indiquemos
+    if(isset($_POST["btnMostrarHorario"])){
+        try{
+            $consulta = "SELECT h.dia, h.hora, g.nombre AS grupo_nombre FROM horario_lectivo h JOIN grupos g ON h.grupo = g.id_grupo WHERE h.usuario = '".$_POST['horario']."'";
+            $result_datos_horario_profesor=mysqli_query($conexion,$consulta);
+
+        // Creamos la matriz del horario
+        $horario = [];
+        while ($fila = mysqli_fetch_assoc($result_datos_horario_profesor)) {
+            $dia = $fila['dia']; 
+            $hora = $fila['hora']; 
+            $grupo = $fila['grupo_nombre']; 
+            $horario[$dia][$hora] = $grupo; // Lo almacenamos
+        }
+        $_SESSION["horario"]=$horario;
+        }catch(Exception $e){
+            die(error_page("Examen2 23-24","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+        }
     }
 
     // Siempre se cierra
@@ -41,42 +62,11 @@
             require "vistas/vista_seleccion.php";
 
             if(isset($_POST["btnMostrarHorario"])){
-                echo "<h3>Horario del Profesor: ".$nombre_profesor."</h3>";
-                $dias[]="";
-                $dias[]="Lunes";
-                $dias[]="Martes";
-                $dias[]="Miércoles";
-                $dias[]="Jueves";
-                $dias[]="Viernes";
-
-                $horas=["8:15-9:15","9:15,10:15","10:15,11:15","11:15,11:45","11:45,12:45","12:45,13:45","13:45,14:45","14:45,15:45"];
-
-                echo "<table>";
-                    echo "<tr>";
-                        for ($i=0; $i < count($dias); $i++) { 
-                            echo "<th>".$dias[$i]."</th>";
-                        }
-                    echo "</tr>";
-                    for ($i=0; $i <count($horas); $i++) { 
-                        echo "<tr>";
-                            echo "<th>".$horas[$i]."</th>";
-                            if($i===3){
-                                echo "<td colspan='5'> Descanso</td>";
-                            }else{
-                            for ($j=1; $j < count($dias); $j++) {
-                                
-                                    echo "<td>Hola</td>";
-                                
-                                }
-                            }
-                        echo "</tr>";
-                    }
-                echo "</table>";
-                
+                require "vistas/vista_tabla.php";
             }
-            
-       
-
+            if(isset($_POST["btnEditar"])){
+                echo "<p> Has pulsdado</p>";
+            }
         ?>
     </body>
 </html>
