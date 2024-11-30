c9e1be7689bc13419deb51864feda9f41f7af6da
Victor <menaflores.victor@gmail.com>
2024-11-30 19:25:48 +0100

    PHP-> casa: practicar

--- PHP/Practicas/Examen_23_24_notas/index.php
+++ PHP/Practicas/Examen_23_24_notas/index.php
@@ -24,15 +24,60 @@
     // Obtenemos los datos del alumno en concreto
     if(isset($_POST["alumno"])){
         try{
-            $consulta="select * from notas where cod_alu='".$_POST["alumno"]."'";
+            $consulta="select notas.cod_asig, notas.nota, asignaturas.denominacion from notas join asignaturas on notas.cod_asig = asignaturas.cod_asig where notas.cod_alu = '".$_POST["alumno"]."'";
             $result_detalle_alumno=mysqli_query($conexion,$consulta);
-            
+            $_SESSION["alumno"]=$_POST["alumno"];
+
         }catch(Exceptions $e){
             session_destroy();
             mysqli_close($conexion);
         die(error_page("Examen notas","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
         }
+
+        //Obtenemos las notas que le faltan
+        try
+        {
+            $consulta="select * from asignaturas where cod_asig not in (select cod_asig from notas where cod_alu='".$_POST["alumno"]."')";
+            $result_asignaturas_faltan=mysqli_query($conexion,$consulta);
+            
+        }
+        catch(Exception $e)
+        {
+            session_destroy();
+            mysqli_close($conexion);
+            die(error_page("Examen2 PHP","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+        }
     }
+
+    if(isset($_POST["btnCalificar"])){
+        try
+        {
+            $consulta="insert into notas (cod_alu, cod_asig, nota) values ('".$_POST["cod_alumno"]."','".$_POST["asignatura"]."','0.00')";
+            $result_asignaturas_faltan=mysqli_query($conexion,$consulta);
+            
+        }
+        catch(Exception $e)
+        {
+            session_destroy();
+            mysqli_close($conexion);
+            die(error_page("Examen2 PHP","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+        }
+    }
+
+    if(isset($_POST["btnBorrar"])){
+        try
+        {
+            $consulta="delete from notas where cod_alu='".$_POST["cod_alu"]."' and cod_asig='".$_POST["cod_asig"]."'";
+            $result_grupos_libres_profesor_dia_hora=mysqli_query($conexion,$consulta);
+        }
+        catch(Exception $e)
+        {
+            session_destroy();
+            mysqli_close($conexion);
+            die(error_page("Examen2 PHP","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+        }
+    }
+    
     //cerramos
     mysqli_close($conexion);
 ?>
--- PHP/Practicas/Examen_23_24_notas/vistas/vista_seleccion.php
+++ PHP/Practicas/Examen_23_24_notas/vistas/vista_seleccion.php
@@ -5,13 +5,14 @@
                 <?php
                     while($tupla=mysqli_fetch_assoc($result_select))
                     {
-                        if(isset($_POST["aulmno"]) && $_POST["alumno"]==$tupla["cod_alu"])
+                        if(isset($_POST["alumno"]) && $_POST["alumno"]==$tupla["cod_alu"])
                         {
                             echo "<option selected value='".$tupla["cod_alu"]."'>".$tupla["nombre"]."</option>";
-                            $nombre_profesor=$tupla["nombre"];
+                            $nombre_alumno=$tupla["nombre"];
                         }
                         else
                             echo "<option value='".$tupla["cod_alu"]."'>".$tupla["nombre"]."</option>";
+                            
                     }
                     mysqli_free_result($result_select);
                 ?>
