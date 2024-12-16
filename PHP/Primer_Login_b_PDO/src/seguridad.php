712d546c6da22d29665b050fb56234d9c409b2d1
vmenflo <vmenflo707@g.educaand.es>
2024-12-16 14:03:04 +0100

    PHP->16/12: primer_login_b_PDO

---
+++ PHP/Primer_Login_b_PDO/src/seguridad.php
@@ -0,0 +1,54 @@
+<?php
+try {
+    $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+} catch (PDOException $e) {
+    session_destroy();
+    die(error_page("Primer Login b", "<p>No se ha podido realizar la consulta: " . $e->getMessage() . "</p>"));
+}
+// Me he conectado y ahora hago la consulta para el baneo
+try
+{
+    $consulta = "select tipo from usuarios where usuario=? AND clave=?";
+    $sentencia = $conexion->prepare($consulta);
+    $sentencia->execute([$_SESSION["usuario"], $_SESSION["clave"]]);
+}
+catch(Exception $e)
+{
+    $conexion=null;
+    session_destroy();
+    die(error_page("Primer Login b","<p>No se ha podido realizar la consulta: ".$e->getMessage()."</p>"));
+}
+
+
+if($sentencia->rowCount()<=0)
+{
+    $sentencia=null;
+    session_unset();//Me deslogueo
+    $_SESSION["mensaje_seguridad"]="Usted ya no se encuentra registrado en la BD";
+    header("Location:".$salto);
+    exit;
+}
+
+
+// He pasado el control de baneo
+// Dejo la conexión abierta y aprovecho para coger datos del usuario logueado
+
+$datos_usuario_log=$sentencia->fetch(PDO::FETCH_ASSOC);
+$sentencia=null;
+
+
+
+// Ahora controlo el tiempo de inactividad
+
+if(time()-$_SESSION["ultm_accion"]>INACTIVIDAD*60)
+{
+    session_unset();//Me deslogueo
+    $_SESSION["mensaje_seguridad"]="Su tiempo de sesión ha expirado. Por favor, vuelva a loguearse";
+    header("Location:".$salto);
+    exit;
+}
+
+$_SESSION["ultm_accion"]=time();
+
+
+?>
