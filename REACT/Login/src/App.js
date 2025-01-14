23107da3038694b87a2c50925fcacf95127523c9
vmenflo <vmenflo707@g.educaand.es>
2025-01-14 10:07:27 +0100

    REACT->14/01: Login

---
+++ REACT/Login/src/App.js
@@ -0,0 +1,43 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component } from 'react';
+import Menu from './componentes/Menu'
+import AppLogin from './componentes/AppLogin'
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuItem: "UNO",
+      logged: false,
+    }
+  }
+
+  changeMenu(item) {
+    this.setState({ menuItem: item })
+  }
+
+  userLogin(telefono, password) {
+    if (telefono == "myfpschool@gmail.com" && password == "2025") {
+      this.setState({ logged: true })
+    }
+
+  }
+  render() {
+    let obj = <><Menu
+      changeMenu={(item) => this.changeMenu(item)} /></>
+
+    if (!this.state.logged) {
+      obj = <AppLogin
+        userLogin={(telefono, password) => this.userLogin(telefono, password)}
+      />
+    }
+
+
+    return (
+      <div className="App">
+        {obj}
+      </div>
+    );
+  }
+
+}
+export default App;
