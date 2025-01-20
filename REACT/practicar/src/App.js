a6e89d257c29f67e441383663ca1f565353ef869
Victor <menaflores.victor@gmail.com>
2025-01-20 19:33:40 +0100

    REACT-> casa: damas

--- REACT/practicar/src/App.js
+++ REACT/practicar/src/App.js
@@ -2,67 +2,104 @@ import React, { Component } from 'react';
 import { Button } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Botonera(props){
-
-  
-  if(props.playable){
+function Botonera(props) {
+  if (props.playable) {
     let lista = [];
 
+    // Suponiendo que `props.listaBotones` es un arreglo bidimensional
     for (let i = 0; i < props.listaBotones.length; i++) {
       for (let j = 0; j < props.listaBotones[i].length; j++) {
-        if(i%2===0){
-          lista.push(<Button color={props.listaBotones[i][j]} />);
-          lista.push(<Button outline />);
-        }else{
-          lista.push(<Button outline />);
-          lista.push(<Button color={props.listaBotones[i][j]} />);
+        // Corregimos la generación de botones
+        if (i % 2 === 0) {
+          lista.push(<Button onClick={()=>props.clicar(i,j)} color={props.listaBotones[i][j].color} key={`btn-${i}-${j}`} />);
+          lista.push(<Button outline key={`btn-out-${i}-${j}`} />);
+        } else {
+          lista.push(<Button outline key={`btn-out-${i}-${j}`} />);
+          lista.push(<Button onClick={()=>props.clicar(i,j)} color={props.listaBotones[i][j].color} key={`btn-${i}-${j}`} />);
         }
       }
-      lista.push(<br/>);
+      lista.push(<br key={`br-${i}`} />);
     }
-    return(<>{lista}</>);
+    return <>{lista}</>;
   }
 }
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      listaBotones: Array(8).fill(Array(8)),
-      playable:false,
+      listaBotones: [], // Inicializamos como array vacío
+      playable: false,
+      fila:-1,
+      columna:-1
     };
   }
 
-  jugar(){
-    let copPlayable = this.state.playable;
-    let copLista = this.state.listaBotones;
+  clicar(i, j) {
+    let copiaLista=this.state.listaBotones
+    let cf = this.state.fila
+    let cc = this.state.columna
 
-    if(copPlayable){
-      copPlayable=false;
+    if(cf!==-1){
+      if (
+        ((i === cf - 1 && (j === cc - 1 || j === cc + 1)) || // Movimientos hacia arriba y diagonal
+        (i === cf + 1 && (j === cc - 1 || j === cc + 1)))  && copiaLista[i][j].libre  // Movimientos hacia abajo y diagonal
+      ){
+        copiaLista[i][j].color="success"
+        copiaLista[i][j].libre=false
+        copiaLista[cf][cc].libre=true
+        copiaLista[cf][cc].color="secondary"
+        cf=-1
+        cc=-1
+      }else{
+        copiaLista[cf][cc].color="success"
+        cf=-1
+        cc=-1
+      }
+
     }else{
-      copPlayable=true;
+      copiaLista[i][j].color="primary";
+      cf=i
+      cc=j
     }
     
-
-    for (let i = 0; i < 5; i++) {
-      copLista[i] = Array(4).fill("secondary");
-    }
-
-    for (let j = 5; j < 8; j++) {
-      copLista[j] = Array(4).fill("success");
-    }
-
-    this.setState({listaBotones:copLista,playable:copPlayable});
-
+    this.setState({listaBotones:copiaLista,fila:cf,columna:cc})
   }
 
-  render(){
+  jugar() {
+    let copPlayable = this.state.playable;
+    let copLista = [];
+
+    if (!copPlayable) {
+      // Creamos la lista de botones dinámicamente como un arreglo bidimensional
+      for (let i = 0; i < 8; i++) {
+        let fila = [];
+        for (let j = 0; j < 4; j++) {
+          if (i < 5) {
+            fila.push({ libre: true, color: "secondary" });
+          } else {
+            fila.push({ libre: true, color: "success" });
+          }
+        }
+        copLista.push(fila); // Agregamos la fila al arreglo principal
+      }
+      copPlayable = true;
+    } else {
+      // Si ya está en modo jugable, reseteamos
+      copLista = [];
+      copPlayable = false;
+    }
+
+    this.setState({ listaBotones: copLista, playable: copPlayable });
+  }
+
+  render() {
     return (
       <div className="App">
         <header className="App-header">
-          <Button onClick={()=>this.jugar()}>Jugar</Button>
-          <br/>
-          <Botonera listaBotones={this.state.listaBotones} playable={this.state.playable}/>
+          <Button onClick={() => this.jugar()}>Jugar</Button>
+          <br />
+          <Botonera clicar={(i,j)=>this.clicar(i,j)} listaBotones={this.state.listaBotones} playable={this.state.playable} />
         </header>
       </div>
     );
