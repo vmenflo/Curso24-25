9d4426ea16c148c436f7573a113b43054aae5e47
Victor <menaflores.victor@gmail.com>
2025-01-27 21:35:44 +0100

    REACT

--- REACT/plantilla/src/App.js
+++ REACT/plantilla/src/App.js
@@ -1,139 +1,110 @@
-import './App.css';
+import React, { Component, useState } from 'react';
+import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState } from 'react';
 
-import { Component } from 'react';
-import {
-  Alert, Row, Col, UncontrolledAccordion, AccordionItem, AccordionHeader, AccordionBody, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label
- } from 'reactstrap';
- 
-const VentanaModal = (props) => {
-  const { className } = props;
 
-  const [total, setTotal] = useState('');
-  const [meses, setMeses] = useState("6");
-  const [tae, setTae] = useState("6");
-  const [info, setInfo] = useState('Inserte los datos');
+const Altas = ({ nuevoUsuario }) => {
+  // UTILICE HOOKS EN ESTE COMPONENTE
+  const [nombre, setNombre]=useState("")
+  const [apellidos, setApellidos]=useState("")
+  const [telefono, setTelefono]=useState("")
 
   const handleChange = (event) => {
-    setInfo('');
-    const target = event.target;
-    if (target.name == "total") {
-        setTotal(target.value);
+    if(event.target.name === "nombre"){
+      setNombre(event.target.value)
     }
-    if (target.name == "meses") {
-        setMeses(target.value);
+    if(event.target.name === "apellidos"){
+      setApellidos(event.target.value)
     }
-    if (target.name == "tae") {
-      setTae(target.value);
-  }
+    if(event.target.name === "telefono"){
+      setTelefono(event.target.value)
+    }
   }
 
-  const pasarDatos = () => {
-    if(total === " "){
-      setInfo("Inserte los datos")
-    }else{
-      setInfo("")
-      props.aceptar(total,meses,tae);
-    }
-      
-    
-  };
- 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    nuevoUsuario(nombre,apellidos,telefono)
+  }
+
   return (
-    <div>
-      <Modal isOpen={props.mostrar} toggle={props.toggle} className={className} onEntering={"//ESTO SE EJECUTA CUANDO MUESTRAS LA VENTANA"}>
-        <ModalHeader toggle={props.toggle}>{props.titulo}</ModalHeader>
-        <ModalBody>
-          <FormGroup row>
-            <Label sm={2} > Cantidad a pedir: </Label>
-            <Col sm={10}>
-              <Input onChange={handleChange}
-                id="total"
-                name="total"
-                type="Text" />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-            <Col sm={12}>
-            <Input onChange={handleChange} onClick={handleChange}
-                id="meses"
-                name="meses"
-                type="number"
-                min={6}
-                max={36}
-                value={meses} 
-              >
-              </Input>
-              <br/>
-              <Input onChange={handleChange} onClick={handleChange}
-                id="tae"
-                name="tae"
-                type="select"
-                value={tae}
-              >
-                <option value={5}>5%</option>
-                <option value={6}>6%</option>
-                <option value={7}>7%</option>
-                <option value={8}>8%</option>
-                <option value={8}>9%</option>
-              </Input>
-              <br/>
-                {info}
-            </Col>
-          </FormGroup>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={pasarDatos}>{props.botonAceptar}</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-        </ModalFooter>
-      </Modal>
-    </div>
+    <Form onSubmit={handleSubmit}>
+      <FormGroup>
+        <Label for="Nombre">Nombre:</Label>
+        <Input
+          name="nombre"
+          id="nombre"
+          placeholder="introduzca nombre"
+          onChange={handleChange}
+        />
+        <Label for="Nombre">Apellidos:</Label>
+        <Input
+          name="apellidos"
+          id="apellidos"
+          placeholder="introduzca apellidos"
+          onChange={handleChange}
+        />
+        <Label for="Nombre">Telefono:</Label>
+        <Input
+          name="telefono"
+          id="telefono"
+          placeholder="introduzca telefono"
+          onChange={handleChange}
+        />
+      </FormGroup>
+      <Button>Añadir</Button>
+    </Form>
   );
 }
 
+const Mostrar = ({contactos}) => {
+  // ESTE COMPONENTE MUESTRA EL LISTÍN TELEFÓNICO.
+
+  return (
+    <>
+      <h3>Listado</h3>
+      <ul>
+        {contactos.map(u=>{
+          return (<li> Nombre: {u.nombre} - Apellidos: {u.apellidos} - Telefono: {u.telefono}</li>)
+        })}
+    </ul>
+    </>
+    
+  );
+}
 
 class App extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      isOpen:false,
-    }
-  }
-  
-  setIsOpen(d){
-    if (d===undefined) return;
-    this.setState({isOpen:d})
-  }
-  
-  aceptar(total,meses,tae){
-      this.toggleModal();
-
-      let r = (tae / 100) /12
-
-      let cuota = (total*r)/(1-Math.pow((1+r),-meses))
-   
-      console.log("La cuota es: "+Math.round(cuota)+" al mes.")
-    
+      // INSERTE AQUÍ EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN DE LA APLICACIÓN. EL LISTÍN TELEFÓNICO
+      contactos: [
+       {nombre:"Victor", apellidos:"Mena", telefono:"123"},
+      ],
+    };
   }
 
-  toggleModal(){this.setIsOpen(!this.state.isOpen)}
+  nuevoUsuario = (nombre,apellidos,telefono) => {
+    let copia = this.state;
+
+    let obj = {}
+    obj.nombre = nombre;
+    obj.apellidos = apellidos;
+    obj.telefono=telefono;
+
+
+    copia.contactos.push(obj)
+    this.setState({copia})
+  }
 
   render() {
     return (
       <div className="App">
-        <Button onClick={()=>{this.toggleModal()}}>
-          Dale
-        </Button>
-        <VentanaModal 
-            aceptar={(total,meses,tae)=>this.aceptar(total,meses,tae)}
-            mostrar={this.state.isOpen}
-            botonAceptar={"Calcular"}
-            titulo={"CALCULO DE TU CUOTA"}
+        <Mostrar
+          contactos={this.state.contactos}
         />
-
+        <Altas nuevoUsuario={(nombre,apellidos,telefono)=>this.nuevoUsuario(nombre,apellidos,telefono)} />
       </div>
     );
   }
 }
-
 export default App;
