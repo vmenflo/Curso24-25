dd4d66027ad3f2b509f76e90ed31a8e76ae2d30f
vmenflo <vmenflo707@g.educaand.es>
2025-02-03 10:07:29 +0100

    REACT-> 3/2

--- REACT/Altas/src/App.js
+++ REACT/Altas/src/App.js
@@ -1,113 +1,134 @@
-import React, { Component, useState } from 'react';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  Alert, Row, Col, UncontrolledAccordion, AccordionItem, AccordionHeader, AccordionBody, Input, Button, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label
- } from 'reactstrap';
+import { Component,useState } from 'react';
+import { Button, Modal, ModalBody, ModalHeader, FormGroup, Label, Col, Input, ModalFooter } from 'reactstrap';
 
-const VentanaModal = (props) => {
-  const { className } = props;
+const Mostrar = (props) => {
+  // ESTE COMPONENTE MUESTRA EL LISTÍN TELEFÓNICO.
+  return (
+    <ul>
+      {props.elementos.map((e) => (
+        <li>
+          {e.nombre}-{e.telefono + "  "}<Button onClick={()=> props.borrar(e.telefono)}>x</Button>
+        </li>
+      ))}
+    </ul>
+  );
+ };
+ 
+
+const VentanaModalListin = (props) => {
+  const {
+    className
+  } = props;
+  const [nombre, setNombre] = useState(undefined);
+  const [telefono, setTelefono] = useState(undefined);
+ 
   const handleChange = (event) => {
-    // COMPLETA ESTA FUNCION
+    if (event.target.name === "nombre") {
+      setNombre(event.target.value.toUpperCase())
+    }
+    if (event.target.name === "telefono") {
+      setTelefono(event.target.value)
+    }
+ 
+  }
+  const click = () => {
+    props.insertaPersona(nombre,telefono)
+    setNombre(undefined)
+    setTelefono(undefined)
+    props.toggle();
   }
 
- 
   return (
     <div>
-      <Modal isOpen={props.mostrar} toggle={props.toggle} className={className} onEntering={"//ESTO SE EJECUTA CUANDO MUESTRAS LA VENTANA"}>
+      <Modal isOpen={props.mostrar} toggle={props.toggle} className={className} onEntering={() => { }}>
         <ModalHeader toggle={props.toggle}>{props.titulo}</ModalHeader>
         <ModalBody>
+
+
           <FormGroup row>
             <Label sm={2} > Nombre: </Label>
             <Col sm={10}>
               <Input onChange={handleChange}
                 id="nombre"
                 name="nombre"
-                type="nombre" />
+                type="Text" />
             </Col>
           </FormGroup>
+
+
           <FormGroup row>
-          <Label sm={2} > Apellidos: </Label>
-            <Col sm={10}>
-              <Input onChange={handleChange}
-                id="apellidos"
-                name="apellidos"
-                type="apellidos" />
-            </Col>
-          </FormGroup>
-          <FormGroup row>
-          <Label sm={2} > Telefono: </Label>
+            <Label sm={2} > Teléfono: </Label>
             <Col sm={10}>
               <Input onChange={handleChange}
                 id="telefono"
                 name="telefono"
-                type="telefono" />
+                type="Text" />
             </Col>
           </FormGroup>
+
+
         </ModalBody>
         <ModalFooter>
-          <Button color="primary" onClick={props.toggle}>Cancelar</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          <Button color="primary" onClick={()=>props.aceptar("NECESITA DATOS")}>Aceptar</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <Button color="primary" onClick={() => click()}>{props.aceptar}</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </ModalFooter>
       </Modal>
     </div>
+
+
   );
 }
 
-const Lista = ({ usuarios }) => {
-  
-  let copia = usuarios;
-
-  return (
-    <>
-      <h2>Listado</h2>
-      <ul>
-      {copia.map(u=> {
-        return(<li>
-          {u.nombre} - {u.apellidos} - {u.telefono} 
-        </li>)
-      })}
-    </ul>
-    </>
-  );
-}
 
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      contactos: [
-       {nombre:"Victor", apellidos:"Mena", telefono:"123"},
-      ],
-      isOpen:false,
+      // INSERTE AQUÍ EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN
+      listaUsuarios: [{nombre:"fulano",telefono:"1232546456"},{nombre:"mengano",telefono:"343"}],
+      isOpen: false,
     };
   }
 
-  abrirAlta(){
-    this.setState({isOpen:true})
+  setIsOpen(d) {
+    if (d === undefined) return;
+    this.setState({ isOpen: d })
   }
 
-  aceptar(datos){
-    console.log(datos)
-    this.toggleModal();
+  borrarUsuario(telefono){
+    let copia = this.state.listaUsuarios
+    copia.filter(u=> u.telefono!==telefono)
+
+    this.setState({listaUsuarios:copia})
   }
 
-  toggleModal(){this.setIsOpen(!this.state.isOpen)}
+  toggleModal() {
+    this.setState({ isOpen: !this.state.isOpen })
+  }
+
+  insertaPersona(n, t) {
+    let p = this.state.listaUsuarios;
+    let newp = { nombre: n, telefono: t };
+    p.push(newp);
+    this.setState({ listaUsuarios: p });
+  }
 
   render() {
     return (
       <div className="App">
-       <Lista usuarios={this.state.contactos}/>
-       <Button onClick={()=>this.abrirAlta()}>Dar de alta</Button>
-       <VentanaModal 
-            aceptar={(datos)=>this.aceptar(datos)}
-            mostrar={this.state.isOpen}
-            botonAceptar={"Calcular"}
-            titulo={"Introduce los datos del USUARIO"}
-            toogle={()=>this.toggleModal()}
+        <Mostrar elementos={this.state.listaUsuarios} borrar={(telefono)=>this.borrarUsuario(telefono)} />
+        <Button color="primary" onClick={() => { this.toggleModal() }}> Alta Usuario </Button>
+        <VentanaModalListin
+          insertaPersona={(nombre, telefono) => this.insertaPersona(nombre, telefono)}
+          mostrar={this.state.isOpen}
+          toggle={() => this.toggleModal()}
+          aceptar={"Alta usuario"}
         />
       </div>
     );
   }
 }
+
 export default App;
